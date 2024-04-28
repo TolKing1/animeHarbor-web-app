70459cc2f766df4934850005c92a09dadd983144
To'lqin Oltiboyev <tolking0101@gmail.com>
2024-04-28 22:35:30 +0500

    INITIAL setup files

---
+++ src/main/java/org/tolking/animeharbor/AnimeHarborApplication.java
@@ -0,0 +1,13 @@
+package org.tolking.animeharbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeHarborApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeHarborApplication.class, args);
+    }
+
+}
