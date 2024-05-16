88823026ba24d80b93c9e008eacff751db859368
To'lqin Oltiboyev <tolking0101@gmail.com>
2024-05-16 13:12:09 +0500

    FIX getter

--- src/main/java/org/tolking/animeharbor/dto/ImageDataDto.java
+++ src/main/java/org/tolking/animeharbor/dto/ImageDataDto.java
@@ -1,9 +1,8 @@
 package org.tolking.animeharbor.dto;
 
-import lombok.Getter;
 import org.springframework.core.io.ByteArrayResource;
 
-@Getter
+
 public record ImageDataDto(ByteArrayResource resource, String mimeType) {
 
 }
